2134475f5ebcce906b8c196cdfbdbb34f5b114e6
riiy zhou <riiyzhou@gmail.com>
2023-12-26 18:13:19 +0800

    save verify_code to user table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import base64
 import contextlib
+import random
 
 import databases
 import jwt
@@ -21,6 +22,7 @@ from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.routing import Mount, Route
+from sqlalchemy.dialects.postgresql import insert
 
 import settings
 from models import metadata, user
@@ -72,35 +74,43 @@ class Login(HTTPEndpoint):
         totp = pyotp.TOTP(b32str, interval=settings.EMAIL_OTP_INTERVAL)
         return totp
 
+    def _send_verify_code_task(self, email, verify_code):
+        """."""
+        send_mail(
+            subject="verify code",
+            content=verify_code,
+            to=[email],
+            sender="Finacial View",
+        )
+
     async def post(self, request):
         req = await request.json()
         email = req["email"]
-        totp = self._gettotp(email)
-        if not totp.verify(req["captcha"]):
-            raise HTTPException(status_code=401)
         value = {"email": email}
-        query = "SELECT email FROM auth.user WHERE email = :email"
+        query = "SELECT verify_code FROM auth.user WHERE email = :email"
         result = await database.fetch_one(query=query, values=value)
         if not result:
             query = user.insert()
             await database.execute(query=query, values=value)
+        if result[0] != req['captcha']:
+            raise HTTPException(status_code=401)
         value["token"] = jwt.encode(value, str(settings.SECRET_KEY), algorithm="HS256")
-        value['type'] = 'email'
-        value['currentAuthority'] = 'user'
+        value["type"] = "email"
+        value["currentAuthority"] = "user"
         return OrjsonResponse(value)
 
     async def get(self, request):
-        email = request.query_params.get('email')
+        email = request.query_params.get("email")
         if not email:
             raise HTTPException(status_code=401)
-        totp = self._gettotp(email)
-        otp_code = totp.now()
+        verify_code = str(random.randint(1000, 9999))
+        insert_stmt = insert(user).values(email=email, verify_code=verify_code)
+        do_update_stmt = insert_stmt.on_conflict_do_update(
+            constraint="user_pkey", set_=dict(verify_code=verify_code)
+        )
+        await database.execute(do_update_stmt)
         task = BackgroundTask(
-            send_mail,
-            subject="verify code",
-            content=otp_code,
-            to=[email],
-            sender="Finacial View",
+            self._send_verify_code_task, email=email, verify_code=verify_code
         )
         message = {"detail": "check email", "interval": settings.EMAIL_OTP_INTERVAL}
         return OrjsonResponse(message, background=task)
@@ -124,6 +134,7 @@ class BasicAuthBackend(AuthenticationBackend):
         email = decoded["email"]
         return AuthCredentials(["authenticated"]), SimpleUser(email)
 
+
 @requires("authenticated")
 async def current_user(request):
     """."""
@@ -132,11 +143,11 @@ async def current_user(request):
     user = {"email": user.display_name}
     return OrjsonResponse(user)
 
+
 login_route = [
     Route("/login/account", endpoint=Login),
     Route("/login/captcha", methods=["GET"], endpoint=Login),
     Route("/current-user", methods=["GET"], endpoint=current_user),
-
 ]
 
 routes = [
